5274f87c705d3adbefc815262b20b0eae0304baa
Ben Armston <ben.armston@alces-software.com>
2023-06-29 11:12:59 +0100

    Better error reporting for heat errors

--- cluster_builder/__init__.py
+++ cluster_builder/__init__.py
@@ -9,6 +9,7 @@ from jsonschema.exceptions import (best_match)
 from werkzeug.exceptions import HTTPException
 import keystoneauth1.exceptions.connection as ks_connection_exceptions
 import keystoneauth1.exceptions.http as ks_http_exceptions
+import heatclient.exc as heat_exceptions
 
 def create_app(instance_path=None, test_config=None):
     # dictConfig({
@@ -54,13 +55,18 @@ def create_app(instance_path=None, test_config=None):
 
     for exc in map(ks_http_exceptions.__dict__.get, ks_http_exceptions.__all__):
         if inspect.isclass(exc) and issubclass(exc, Exception):
-            app.logger.debug(f"creating error handler for exception {exc}")
+            app.logger.debug(f"creating error handler for {exc.__module__}.{exc.__qualname__}")
             app.register_error_handler(exc, _handle_keystone_http_exception)
 
     for exc in map(ks_connection_exceptions.__dict__.get, ks_connection_exceptions.__all__):
         if inspect.isclass(exc) and issubclass(exc, Exception):
-            app.logger.debug(f"creating error handler for exception {exc}")
+            app.logger.debug(f"creating error handler for {exc.__module__}.{exc.__qualname__}")
             app.register_error_handler(exc, _handle_keystone_connection_exception)
+
+    for exc in map(heat_exceptions.__dict__.get, heat_exceptions.__dict__):
+        if inspect.isclass(exc) and issubclass(exc, heat_exceptions.HTTPException):
+            app.logger.debug(f"creating error handler for {exc.__module__}.{exc.__qualname__}")
+            app.register_error_handler(exc, _handle_heat_http_exception)
 
     @app.errorhandler(400)
     def bad_request(error):
@@ -128,3 +134,13 @@ def _handle_keystone_connection_exception(error):
     title = error.__class__.__name__
     body = [{"status": "502", "title": title, "detail": error.message}]
     return make_response(jsonify(body), 502)
+
+
+def _handle_heat_http_exception(error):
+    current_app.logger.debug(f"handling error {error.__class__} with _handle_heat_http_exception")
+    current_app.logger.info(str(error))
+    original_error = error.error
+    title = original_error['title']
+    detail = original_error['error']['message']
+    body = [{"status": str(error.code), "title": title, "detail": detail}]
+    return make_response(jsonify(body), error.code)
