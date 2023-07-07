16ab3e8cd0619bb8a6f42e94e148b7b9a57eec42
timalces <tim.brackfield@alces-software.com>
2023-07-07 16:42:02 +0100

    naive implementation of if modified since for cluster types index

--- cluster_builder/cluster_types.py
+++ cluster_builder/cluster_types.py
@@ -1,4 +1,7 @@
 from flask import Blueprint
+from flask import make_response
+from flask import request
+from dateutil import parser
 
 from .models import ClusterType
 
@@ -6,8 +9,16 @@ bp = Blueprint('cluster-types', __name__, url_prefix="/cluster-types")
 
 @bp.route('/')
 def index():
-    attributes = ["id", "title", "description", "parameters"]
+    attributes = ["id", "title", "description", "parameters", "last_updated"]
     cluster_types = []
+    last_modified = ""
     for ct in ClusterType.all():
+        if last_modified == "" or ct.last_updated > last_modified: last_modified = ct.last_updated
         cluster_types.append(ct.asdict(attributes))
-    return cluster_types
+    if (request.if_modified_since and last_modified != "" and
+       int(request.if_modified_since.timestamp()) >= int(last_modified.timestamp())):
+       return '', 304
+
+    r = make_response(cluster_types)
+    r.last_modified = last_modified
+    return r
