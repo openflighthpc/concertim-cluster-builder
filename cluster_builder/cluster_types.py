f4511293bca22f0d8b67694615d9146b5ff24a9c
timalces <tim.brackfield@alces-software.com>
2023-07-12 15:33:00 +0100

    added show individual cluster type route

--- cluster_builder/cluster_types.py
+++ cluster_builder/cluster_types.py
@@ -6,19 +6,31 @@ from dateutil import parser
 from .models import ClusterType
 
 bp = Blueprint('cluster-types', __name__, url_prefix="/cluster-types")
+ATTRIBUTES = ["id", "title", "description", "parameters", "last_modified"]
 
 @bp.route('/')
 def index():
-    attributes = ["id", "title", "description", "parameters", "last_modified"]
     cluster_types = []
     last_modified = ""
     for ct in ClusterType.all():
         if last_modified == "" or ct.last_modified > last_modified: last_modified = ct.last_modified
-        cluster_types.append(ct.asdict(attributes))
+        cluster_types.append(ct.asdict(ATTRIBUTES))
     if (request.if_modified_since and last_modified != "" and
        int(request.if_modified_since.timestamp()) == int(last_modified.timestamp())):
        return '', 304
 
     r = make_response(cluster_types)
     r.last_modified = last_modified
+    return r
+
+@bp.route('/<string:id>')
+def show_cluster_type(id):
+    type = ClusterType.find(id)
+    last_modified = type.last_modified
+    if (request.if_modified_since and last_modified != "" and
+           int(request.if_modified_since.timestamp()) == int(last_modified.timestamp())):
+           return '', 304
+
+    r = make_response(type.asdict(ATTRIBUTES))
+    r.last_modified = type.last_modified
     return r
