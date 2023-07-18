70ae5df2ddf8b67c5faeb625b604fa4fd3623988
timalces <tim.brackfield@alces-software.com>
2023-07-18 16:23:55 +0100

    use None instead of empty string

--- cluster_builder/cluster_types.py
+++ cluster_builder/cluster_types.py
@@ -11,11 +11,11 @@ ATTRIBUTES = ["id", "title", "description", "parameters", "last_modified"]
 @bp.route('/')
 def index():
     cluster_types = []
-    last_modified = ""
+    last_modified = None
     for ct in ClusterType.all():
-        if last_modified == "" or ct.last_modified > last_modified: last_modified = ct.last_modified
+        if last_modified == None or ct.last_modified > last_modified: last_modified = ct.last_modified
         cluster_types.append(ct.asdict(ATTRIBUTES))
-    if (request.if_modified_since and last_modified != "" and
+    if (request.if_modified_since and last_modified != None and
        int(request.if_modified_since.timestamp()) == int(last_modified.timestamp())):
        return '', 304
 
@@ -27,8 +27,7 @@ def index():
 def show_cluster_type(id):
     type = ClusterType.find(id)
     last_modified = type.last_modified
-    if (request.if_modified_since and last_modified != "" and
-           int(request.if_modified_since.timestamp()) == int(last_modified.timestamp())):
+    if (request.if_modified_since and int(request.if_modified_since.timestamp()) == int(last_modified.timestamp())):
            return '', 304
 
     r = make_response(type.asdict(ATTRIBUTES))
