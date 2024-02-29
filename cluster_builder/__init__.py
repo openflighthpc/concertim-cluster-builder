469b343db79581a35dfc678bd78774b8b4f146c5
Ben Armston <ben.armston@alces-flight.com>
2024-02-29 13:26:04 +0000

    Remove reference to unused hot directory

--- cluster_builder/__init__.py
+++ cluster_builder/__init__.py
@@ -41,7 +41,6 @@ def create_app(instance_path=None, test_config=None):
     os.makedirs(app.instance_path, exist_ok=True)
     os.makedirs(os.path.join(app.instance_path, "cluster-types-enabled"), exist_ok=True)
     os.makedirs(os.path.join(app.instance_path, "cluster-types-available"), exist_ok=True)
-    os.makedirs(os.path.join(app.instance_path, "hot"), exist_ok=True)
 
     from .models import ClusterTypeRepo
     ClusterTypeRepo.configure(
